be1ea92da10396e6b3fcda7117a1f2d4f5f4b653
Marco <marco238cuba@gmail.com>
2018-02-13 18:40:29 +0100

    fix: refactor code

--- fairpay/configs/passport.config.js
+++ fairpay/configs/passport.config.js
@@ -1,20 +1,22 @@
 const User = require('../models/user.model');
 const LocalStrategy = require('passport-local').Strategy;
 // const FBStrategy = require('passport-facebook').Strategy;
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //
-// const DEFAULT_USERNAME = 'Anonymous Coward';
+const DEFAULT_USERNAME = 'Anonymous Coward';
 //
 // const FB_CLIENT_ID = process.env.FB_CLIENT_ID || '';
 // const FB_CLIENT_SECRET = process.env.FB_CLIENT_SECRET || '';
 // const FB_CB_URL = '/auth/fb/cb';
 //
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
-// const GOOGLE_CB_URL = '/auth/google/cb';
-//
-// const FB_PROVIDER = 'facebook';
-// const GOOGLE_PROVIDER = 'google';
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID ||
+    '387291381034-jvfb284fkvnkltau3pc0f3lr7a3sur51.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET ||
+    'xMON23EBA0svNequqXs77uZX';
+const GOOGLE_CB_URL = '/auth/google/cb';
+
+const FB_PROVIDER = 'facebook';
+const GOOGLE_PROVIDER = 'google';
 
 module.exports.setup = (passport) => {
 
@@ -88,43 +90,41 @@ module.exports.setup = (passport) => {
  //        profileFields: ['id', 'emails']
  //    }, authenticateOAuthUser));
  //
- //    passport.use('google-auth', new GoogleStrategy({
- //        clientID: GOOGLE_CLIENT_ID,
- //        clientSecret: GOOGLE_CLIENT_SECRET,
- //        callbackURL: GOOGLE_CB_URL
- //    }, authenticateOAuthUser));
- //
- //    function authenticateOAuthUser(accessToken, refreshToken, profile, next) {
- //        let provider;
- //        if (profile.provider === FB_PROVIDER) {
- //            provider = 'facebookId'
- //        } else if (profile.provider === GOOGLE_PROVIDER) {
- //            provider = 'googleId';
- //        } else {
- //            next();
- //        }
- //        User.findOne({ [`social.${provider}`]: profile.id })
- //            .then(user => {
- //                if (user) {
- //                    next(null, user);
- //                } else {
- //                    const email = profile.emails ? profile.emails[0].value : null;
- //                    user = new User({
- //                        username: email || DEFAULT_USERNAME,
- //                        password: Math.random().toString(36).slice(-8), // FIXME: insecure, use secure random seed
- //                        social: {
- //                            [provider]: profile.id
- //                        }
- //                    });
- //                    console.log
- //                    user.save()
- //                        .then(() => {
- //                            next(null, user);
- //                        })
- //                        .catch(error => next(error));
- //                }
- //            })
- //            .catch(error => next(error));
- //    }
- // }
- //
+    // passport.use('google-auth', new GoogleStrategy({
+    //     clientID: GOOGLE_CLIENT_ID,
+    //     clientSecret: GOOGLE_CLIENT_SECRET,
+    //     callbackURL: GOOGLE_CB_URL
+    // }, authenticateOAuthUser));
+    //
+    // function authenticateOAuthUser(accessToken, refreshToken, profile, next) {
+    //     let provider;
+    //     if (profile.provider === FB_PROVIDER) {
+    //         provider = 'facebookId';
+    //     } else if (profile.provider === GOOGLE_PROVIDER) {
+    //         provider = 'googleId';
+    //     } else {
+    //         next();
+    //     }
+//         User.findOne({[`social.${provider}`]: profile.id})
+//             .then(user => {
+//                 if (user) {
+//                     next(null, user);
+//                 } else {
+//                     const email = profile.emails ? profile.emails[0].value : null;
+//                     user = new User({
+//                         username: email || DEFAULT_USERNAME,
+//                         password: Math.random().toString(36).slice(-8), // FIXME: insecure, use secure random seed
+//                         social: {
+//                             [provider]: profile.id
+//                         }
+//                     });
+//
+//                     user.save()
+//                         .then(() => {
+//                             next(null, user);
+//                         })
+//                         .catch(error => next(error));
+//                 }
+//             })
+//             .catch(error => next(error));
+// }
